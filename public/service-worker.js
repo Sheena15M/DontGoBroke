7b31b179fc3efe88c1323f6e38f7c1310d9ef089
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-03 13:04:29 -0500

    code added to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+//This is the service worker. This file will allow for the app to be used offline. 
+//Implenting the cache will make sure the data is stored when it's used offline so it can be accessed online too
+//Use two caches, one for the front end files and one for the routes/api
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -6,4 +9,10 @@ const FILES_TO_CACHE = [
     "/assets/js/db.js",
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-512x512.png",
-]
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
